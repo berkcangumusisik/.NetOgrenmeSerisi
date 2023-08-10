978ed01afd504864a1ebde598a2fcb0bb2bfdacd
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2023-08-10 16:33:48 +0300

    Data Types Started

---
+++ Bölüm 1 C#/03.DataTypes/Program.cs
@@ -0,0 +1,38 @@
+﻿byte sayi = 5;
+Console.WriteLine(sayi);
+double kdvOrani = 0.18;
+Console.WriteLine(kdvOrani);
+char karakter = 'A';
+Console.WriteLine(karakter);
+String ad = "Ahmet";
+Console.WriteLine(ad);
+bool dogruMu = true;
+Console.WriteLine(dogruMu);
+
+
+int x = 5;
+int y = 10;
+x = y;
+y = 20;
+Console.WriteLine(x);
+Console.WriteLine(y);
+/**
+ Veri Türleri
+ - Value Types (Değer Tipleri) : int, float, double, decimal, bool, char, byte, short, long, struct, enum
+ - Reference Types (Referans Tipleri) : string, class, interface, object, dynamic
+ Tam Sayı Veri Tipleri
+- int : 32 bit  
+- long : 64 bit en büyük sayısal veri tipi
+- short : 16 bit -32768 - 32767
+- byte : 8 bit 0-255
+ Ondalıklı Sayı Veri Tipleri
+- float : 32 bit
+- double : 64 bit   
+- decimal : 128 bit en büyük ondalıklı sayısal veri tipi
+    Mantıksal Veri Tipleri
+- bool : true, false
+Char Veri Tipi => Tek karakter tutar ve tek tırnak ile tanımlanır.
+
+var anahtar kelimesi ile değişken tanımlanabilir. Ancak var ile değişken tanımlamak yerine veri tipini belirtmek daha doğrudur.
+ */
+
