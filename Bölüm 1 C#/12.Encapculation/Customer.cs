45b4f11814627a9c235fe1144ec5d909fca93f7f
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2023-09-09 16:17:57 +0300

    Encapculation Added.

---
+++ Bölüm 1 C#/12.Encapculation/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Encapculation
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        private String _firstName;
+        public String FirstName {
+            get {
+                return FirstName;
+            } set
+            { _firstName = value;
+            }
+        }
+        public String LastName { get; set; }
+        public String EmailAddress { get; set; }
+,    }
+}
---
+++ Bölüm 1 C#/12.Encapculation/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _12.Encapculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Ahmet";
+            customer.LastName = "Ahmet";
+            customer.EmailAddress = "abc@gmail.com";
+
+        }
+
+    }
+}
+/**
+ * Encapsulation (Kapsülleme)
+ * Genel olarak bir nesnenin içerisindeki verilerin dışarıdan erişime kapalı olmasıdır.
+ * get ve set metotları ile erişim sağlanır.
+ * get : Database veya farklı bir veri kaynağından veri okunur. Datayı gösterdiğimiz kısım.
+ * set : Dış dünyadan alınan datanın içeride private olarak saklanan field içerisinde değer atandığı kısım. veri kaynağına yazma işlemi.
+ */
